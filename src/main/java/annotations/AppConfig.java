8582d3bbac3b4d6c7f3ef57d75a60909ec70712b
Bastien <bchevriaut@gmail.com>
2020-07-30 10:53:08 +0200

    removing appConfig.java beans and using @components and @ComponentScan

--- src/main/java/annotations/AppConfig.java
+++ src/main/java/annotations/AppConfig.java
@@ -1,20 +1,23 @@
 package annotations;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages="annotations")
 public class AppConfig {
 	
-	@Bean
-	public Samsung getPhone() {
-		return new Samsung();
-	}
-	
-	@Bean
-	public MobileProcessor getProcessor() {
-		return new Snapdragon();
-	}
+//	useless with using @component and @qualifier annotations
+//	@Bean
+//	public Samsung getPhone() {
+//		return new Samsung();
+//	}
+//	
+//	@Bean
+//	public MobileProcessor getProcessor() {
+//		return new Snapdragon();
+//	}
 	
 	
 }
--- src/main/java/annotations/Snapdragon.java
+++ src/main/java/annotations/Snapdragon.java
@@ -1,12 +1,13 @@
 package annotations;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Snapdragon implements MobileProcessor{
 
-	
 	@Override
 	public void process() {
 		System.out.println("I'm Snapdragon Processor");
 	}
-
-
+	
 }
